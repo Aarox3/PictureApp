43359ed6cf51c767ca952865c484b802870166cf
Aarox3 <poiuytrewq1990@o2.pl>
2021-07-06 20:28:17 +0200

    Final version

---
+++ PictureApp/PictureApp/Main.Designer.cs
@@ -0,0 +1,91 @@
+﻿
+namespace PictureApp
+{
+    partial class Main
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.PictureBox = new System.Windows.Forms.PictureBox();
+            this.btnAdd = new System.Windows.Forms.Button();
+            this.btnDelete = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.PictureBox)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // PictureBox
+            // 
+            this.PictureBox.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.PictureBox.Location = new System.Drawing.Point(114, 113);
+            this.PictureBox.Name = "PictureBox";
+            this.PictureBox.Size = new System.Drawing.Size(813, 406);
+            this.PictureBox.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.PictureBox.TabIndex = 0;
+            this.PictureBox.TabStop = false;
+            // 
+            // btnAdd
+            // 
+            this.btnAdd.Location = new System.Drawing.Point(57, 34);
+            this.btnAdd.Name = "btnAdd";
+            this.btnAdd.Size = new System.Drawing.Size(158, 23);
+            this.btnAdd.TabIndex = 1;
+            this.btnAdd.Text = "dodaj zdjęcie...";
+            this.btnAdd.UseVisualStyleBackColor = true;
+            this.btnAdd.Click += new System.EventHandler(this.btnAdd_Click);
+            // 
+            // btnDelete
+            // 
+            this.btnDelete.Location = new System.Drawing.Point(754, 34);
+            this.btnDelete.Name = "btnDelete";
+            this.btnDelete.Size = new System.Drawing.Size(188, 23);
+            this.btnDelete.TabIndex = 2;
+            this.btnDelete.Text = "usuń zdjęcie";
+            this.btnDelete.UseVisualStyleBackColor = true;
+            this.btnDelete.Click += new System.EventHandler(this.btnDelete_Click);
+            // 
+            // Main
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1042, 625);
+            this.Controls.Add(this.btnDelete);
+            this.Controls.Add(this.btnAdd);
+            this.Controls.Add(this.PictureBox);
+            this.Name = "Main";
+            this.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.PictureBox)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox PictureBox;
+        private System.Windows.Forms.Button btnAdd;
+        private System.Windows.Forms.Button btnDelete;
+    }
+}
+
---
+++ PictureApp/PictureApp/Main.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PictureApp
+{
+    public partial class Main : Form
+    {
+        private String imagePath = $@"{ Environment.CurrentDirectory}\imagePath.txt";
+
+        public Main()
+        {
+
+            InitializeComponent();
+
+            ReadFilePath();
+
+            DisplayDeleteButton();
+                                               
+        }
+
+        public void btnAdd_Click(object sender, EventArgs e)
+        {
+
+            try
+            {
+                OpenFileDialog dialog = new OpenFileDialog();
+
+                dialog.Filter = "jpg files(*.jpg) |*.jpg| PNG files(*png) |*.png| All Files(*.*) |*.*";
+
+
+                if (dialog.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+                {
+                  
+                    String filePath = dialog.FileName;
+
+                    PictureBox.ImageLocation = filePath;
+
+                    File.WriteAllText(imagePath, filePath);
+
+                    PictureBox.Refresh();
+
+                    DisplayDeleteButton();
+
+                }
+            
+            }
+
+            catch (Exception)
+            {
+                MessageBox.Show("Błąd!", "Błąd", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+
+
+        }
+
+        public void btnDelete_Click(object sender, EventArgs e)
+        {
+
+            var confirmDelete = MessageBox.Show("Czy na pewno chcesz usunąć to zdjęcie?", "Ostrzeżenie", MessageBoxButtons.YesNo, MessageBoxIcon.Warning);
+
+            if (confirmDelete == DialogResult.Yes)
+
+            {
+
+                PictureBox.ImageLocation = "";
+                               
+                File.WriteAllText(imagePath, "");
+
+                PictureBox.Refresh();
+
+                DisplayDeleteButton();
+
+            }
+            
+        }
+
+
+        public void DisplayDeleteButton()
+        {
+            btnDelete.Enabled = PictureBox.ImageLocation != "";
+
+        }
+
+        public void ReadFilePath()
+        {
+            try
+            {
+                var fileContent = File.ReadAllText(imagePath);
+
+                PictureBox.ImageLocation = fileContent;
+
+                PictureBox.Refresh();
+
+                DisplayDeleteButton();
+            }
+
+            catch(FileNotFoundException e)
+            {
+                    MessageBox.Show(e.Message, "Błąd!", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+
+        }
+    }
+}
+    
+       
+       
+
+
+    
+
